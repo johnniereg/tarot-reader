fe2f4de636f414601d9264b77ed009473d36d448
Johnnie Regalado <johnniereg@gmail.com>
2017-11-10 05:54:05 +0000

    Can pull from Major Arcana. Need to set-up Minor Arcana still.

--- tarot.js
+++ tarot.js
@@ -13,7 +13,30 @@
 // More complicated cards.
 //
 
-
+const theDeck = {
+  0: 'The Fool',
+  1: 'The Magician',
+  2: 'The High Priestess',
+  3: 'The Empress',
+  4: 'The Emperor',
+  5: 'The Hierophant',
+  6: 'The Lovers',
+  7: 'The Chariot',
+  8: 'Strength',
+  9: 'The Hermit',
+  10: 'Wheel of Fortune',
+  11: 'Justice',
+  12: 'The Hanged Man',
+  13: 'Death',
+  14: 'Temperance',
+  15: 'The Devil',
+  16: 'The Tower',
+  17: 'The Star',
+  18: 'The Moon',
+  19: 'The Sun',
+  20: 'Judgement',
+  21: 'The World'
+};
 
 
 function getRandomInt(min, max) {
@@ -23,10 +46,8 @@ function getRandomInt(min, max) {
 }
 
 function pullTarotCard() {
-  let card = getRandomInt(0, 71);
-
-
-  console.log("Your card is: ", card);
+  let card = getRandomInt(0, 21);
+  console.log("Your card is: ", theDeck[card]);
 }
 
 pullTarotCard();
